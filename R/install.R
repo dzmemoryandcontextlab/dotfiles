fc3dd04c51679ebfe86cb655dea5cfbbd44c80f2
Halle Dimsdale-Zucker <zhallez@gmail.com>
2024-06-19 17:41:10 -0700

    Add R config

---
+++ R/install.R
@@ -0,0 +1,14 @@
+install.packages(c(
+                   "cowplot",
+                   "devtools",
+                   "htmltools",
+                   "kableExtra",
+                   "knitr",
+                   "rmarkdown",
+                   "roxygen2",
+                   "svglite",
+                   "tictoc",
+                   "tidyverse",
+                   "yaml"))
+
+devtools::install_github("hallez/halle", subdir="halle")
